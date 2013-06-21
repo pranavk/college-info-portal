7786db265f19a786baa9f1aaefc5208c6f170733
Pranav Kant <pranav913@gmail.com>
2013-06-21 19:42:43 +0530

    set session expiry date to 5 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Nith::Application.config.session_store :cookie_store, :key => '_nith_session'
+Nith::Application.config.session_store :cookie_store, :key => '_nith_session', :expire_after => 5.minutes
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
